7b9587f1b4fc82f119ff3fb1ed0c3aff2da52020
Álvaro Lázaro Gallego <alvaro.gallego@deliveryhero.com>
2015-04-23 18:33:16 +0200

    Add static links and refresh button

--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+import md5
 import os
 import random
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, abort
 import pytz
-
-
 app = Flask(__name__)
+
+
+img_hash_cache = {
+    True: {},
+    False: {},
+}
 
 
 TRANSLATIONS = {
@@ -23,18 +28,32 @@ TRANSLATIONS = {
 }
 
 
+@app.route("/<img_hash>")
 @app.route("/")
-def index():
+def index(img_hash=None):
+    if img_hash == 'favicon.ico':
+        return abort(404)
     is_thursday = datetime.now(
         pytz.timezone('Europe/Madrid')).isoweekday() == 4
-    img = get_image(is_thursday)
-    return render_template(
-        'index.html',
-        **{
-            'is_thursday': is_thursday and _('YES') or _('NO'),
-            'img': img
-        }
-    )
+    if not img_hash:
+        img_hash, img = get_image(is_thursday)
+        return redirect(url_for('index', img_hash=img_hash))
+
+    else:
+        img = get_img_from_hash(img_hash, is_thursday)
+        return render_template(
+            'index.html',
+            **{
+                'is_thursday': is_thursday and _('YES') or _('NO'),
+                'img': img
+            }
+        )
+
+
+def get_img_from_hash(img_hash, is_thursday):
+    img_path = img_hash_cache[is_thursday].get(
+        img_hash, get_image(is_thursday))
+    return img_path
 
 
 def _(string):
@@ -73,7 +92,10 @@ def get_image(is_thursday):
     images = [f for f in os.listdir(os.path.join(root, path))
               if f[-3:] in ['jpg', 'png', 'gif']]
 
-    return '%s/%s' % (path, random.choice(images))
+    img_path = '%s/%s' % (path, random.choice(images))
+    img_hash = md5.md5(img_path.decode('utf-8')).hexdigest()[:6]
+    img_hash_cache[is_thursday][img_hash] = img_path
+    return img_hash, img_path
 
 if __name__ == "__main__":
     app.run()
